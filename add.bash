87d0476d646f7d3b0bde32598bd556664c55b3d3
priscillafrema <pfrema@ucmerced.edu>
2020-12-13 22:25:44 -0800

    First commit

---
+++ add.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+number1=$1
+number2=$2
+number3=$3
+let sum=number1+number2+number3
+let sum*=12
+echo $sum
---
+++ headtail.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+intregex='^[0-9]+$'
+if [ ! -f $1 ]; then
+echo "No file found"
+exit 1
+fi
+if ! [[ $2 =~ $intregex ]]; then
+echo "Second argument must be an integer"
+exit 1
+else
+	echo "Head:"
+	head -n $2 $1
+	echo "Tail:"
+	tail -n $2 $1
+fi
+
